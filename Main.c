c3be77e3910aacbb809f05d2d6bb38470c7adf1e
moujan mirjalili <moujanmirjalili@gmail.com>
2020-01-26 19:04:43 +0330

    so mange changes

--- Main.c
+++ Main.c
@@ -1,17 +1,19 @@
 #include "includes.h"
 #include "funcs.h"
 
-char symbolp1[] = "X";
-char symbolp2[] = "Y";
+char symbolp1[] = " X ";
+char symbolp2[] = " Y ";
 
 Cell* head = NULL;
 Cell* addCell (int x,int y, int team);
 bool access(int n,Tile map[n][n] , int x, int y);
-char * getSymbol(int x , int y);
+char * getSymbol(int x , int y , int n , Tile map[n][n]);
+Cell* showList();
+movePlayer(int n,Tile map[n][n],Cell * movingCell);
 
 void printMap (int n,Tile map[n][n]);
 
-void startGame(int n,int map[n][n],bool singlePlayer,bool loaded){
+void startGame(int n,Tile map[n][n],bool singlePlayer,bool loaded){
 	if (!loaded){
 		int cellNum = nextInt();
 		count(cellNum){
@@ -29,7 +31,41 @@ void startGame(int n,int map[n][n],bool singlePlayer,bool loaded){
 	while(1){
 		clear
 		printMap(n,map);
-		
+		Cell* movingCell = showList();
+		printMoves
+		int play = nextInt();
+		if (play == 1){
+			movePlayer(n,map,movingCell);
+		}
+		if (play == 2){
+			if(checkEmpty(n,map,movingCell->x,movingCell->y) == 0)
+				printf ("Split Failed!");
+			else{
+				addCell(movingCell->x,movingCell->y,movingCell->team);
+				Cell * cell = addCell(movingCell->x,movingCell->y,movingCell->team);
+				randomMove(n,map,cell);
+				removeCell(movingCell);
+			}
+		}
+		if (play == 3){
+			if (map[movingCell->x][movingCell->y].type == Energy && map[movingCell->x][movingCell->y].energy != 0){
+				if (map[movingCell->x][movingCell->y].energy < 15){
+					movingCell->energy += map[movingCell->x][movingCell->y].energy;
+					map[movingCell->x][movingCell->y].energy = 0;
+				}
+				else{
+					movingCell->energy += 15;
+					map[movingCell->x][movingCell->y].energy -= 15;
+				}
+				if (movingCell->energy > 100){
+					map[movingCell->x][movingCell->y].energy += movingCell->energy - 100;
+					movingCell->energy = 100;
+				}
+			}
+			else{
+				printf("Boost Energy Failed!");
+			}
+		}	
 	}
 }
 
@@ -105,7 +141,7 @@ void printMap(int n,Tile map[n][n]){
 		}
 		printf("\n");
 		for ( int j=0;j<n;j+=2){
-			printf("| %s |     " , getSymbol(i,j));
+			printf("|%s|     " , getSymbol(i,j,n,map));
 		}
 		printf("\n");
 		for ( int j=0;j<n;j+=2){
@@ -117,7 +153,7 @@ void printMap(int n,Tile map[n][n]){
 		}
 		printf("\n");
 		for ( int j=1;j<n;j+=2){
-			printf("     | %s |" , getSymbol(i,j));
+			printf("     |%s|" , getSymbol(i,j,n,map));
 		}
 		printf("\n");
 		for ( int j=1;j<n;j+=2){
@@ -128,8 +164,9 @@ void printMap(int n,Tile map[n][n]){
 	}
 }
 
-char * getSymbol(int x , int y){
+char * getSymbol(int x , int y,int n,Tile map[n][n]){
 	Cell * current =head;
+	if(map[x][y].type == Forbidden)return "\\|/";
 	while(current != NULL){
 		if(current->x == x && current->y == y){
 			if(current->team == 1)
@@ -139,6 +176,172 @@ char * getSymbol(int x , int y){
 		}
 		current = current->next;
 	}
-	return " ";
+	return "   ";
 }
+	
+Cell* showList(){
+	Cell* current = head;
+	int i=1;
+	while (current != NULL){
+		printf ("%d) %s (%d,%d) | Energy: %d\n",i,current->name,current->x,current->y,current->energy);
+		i++;
+		current = current->next;
+	}
+	int chooseCell = nextInt();	
+	i=1;
+	Cell* find = head;
+	while (i != chooseCell){
+		find = find->next;
+		i++;
+	}
+	return find;
+}
+
+void movePlayer(int n,Tile map[n][n],Cell * movingCell){
+	compass
+	startMove:;
+	int dir = nextInt();
+		if (dir == North && access(n,map,movingCell->x-1,movingCell->y))
+			movingCell->x=movingCell->x-1;
+		else if (dir == South && access(n,map,movingCell->x+1,movingCell->y))
+			movingCell->x=movingCell->x+1;
+		else if (dir == NorthEast){
+			if(movingCell->y %2 == 0 && access(n,map,movingCell->x-1,movingCell->y+1)){
+				movingCell->x=movingCell->x-1;
+				movingCell->y=movingCell->y+1;
+			}
+			else if(movingCell->y %2 == 1 && access(n,map,movingCell->x,movingCell->y+1)){
+				movingCell->y=movingCell->y+1;
+			}
+			else{
+				printf("Wrong Direction!\n");
+				goto startMove;
+			}	
+		}
+		else if (dir == NorthWest){
+			if(movingCell->y %2 == 0 && access(n,map,movingCell->x-1,movingCell->y-1)){
+				movingCell->x=movingCell->x-1;
+				movingCell->y=movingCell->y-1;
+			}
+			else if(movingCell->y %2 == 1 && access(n,map,movingCell->x,movingCell->y-1)){
+				movingCell->y=movingCell->y-1;
+			}
+			else{
+				printf("Wrong Direction!\n");
+				goto startMove;
+			}
+		}
+		else if (dir == SouthEast){
+			if(movingCell->y %2 == 1 && access(n,map,movingCell->x-1,movingCell->y+1)){
+				movingCell->x=movingCell->x+1;
+				movingCell->y=movingCell->y+1;
+			}
+			else if(movingCell->y %2 == 0 && access(n,map,movingCell->x,movingCell->y+1)){
+				movingCell->y=movingCell->y+1;
+			}
+			else{
+				printf("Wrong Direction!\n");
+				goto startMove;
+			}
+		}
+		else if (dir == SouthWest){
+			if(movingCell->y %2 == 1 && access(n,map,movingCell->x-1,movingCell->y-1)){
+				movingCell->x=movingCell->x+1;
+				movingCell->y=movingCell->y-1;
+			}
+			else if(movingCell->y %2 == 0 && access(n,map,movingCell->x,movingCell->y-1)){
+				movingCell->y=movingCell->y-1;
+			}
+			else{
+				printf("Wrong Direction!\n");
+				goto startMove;
+			}
+		}
+		else{
+			printf("Wrong Direction!\n");
+			goto startMove;
+		}
+}
+
+bool checkEmpty(int n,Tile map[n][n],int x,int y){
+	int empty=0;
+	if (y%2 == 1){
+		if (access(n,map,x,y-1) == true) empty=1;
+		if (access(n,map,x,y+1) == true) empty=1;
+		if (access(n,map,x-1,y) == true) empty=1;
+		if (access(n,map,x+1,y-1) == true) empty=1;
+		if (access(n,map,x+1,y+1) == true) empty=1;
+		if (access(n,map,x+1,y) == true) empty=1;
+	}
+	else{
+		if (access(n,map,x,y-1) == true) empty=1;
+		if (access(n,map,x,y+1) == true) empty=1;
+		if (access(n,map,x-1,y) == true) empty=1;
+		if (access(n,map,x-1,y-1) == true) empty=1;
+		if (access(n,map,x-1,y+1) == true) empty=1;
+		if (access(n,map,x+1,y) == true) empty=1;
+	}
+	return empty;
+}
+
+void randomMove(int n,Tile map[n][n],Cell * movingCell){
+	startMove:;
+	while(1){
+		int dir = rand()%6 +1;
+			if (dir == North && access(n,map,movingCell->x-1,movingCell->y)){
+				movingCell->x=movingCell->x-1;
+				break;
+			}
+			else if (dir == South && access(n,map,movingCell->x+1,movingCell->y)){
+				movingCell->x=movingCell->x+1;
+				break;
+			}
+			else if (dir == NorthEast){
+				if(movingCell->y %2 == 0 && access(n,map,movingCell->x-1,movingCell->y+1)){
+					movingCell->x=movingCell->x-1;
+					movingCell->y=movingCell->y+1;
+					break;
+				}
+				else if(movingCell->y %2 == 1 && access(n,map,movingCell->x,movingCell->y+1)){
+					movingCell->y=movingCell->y+1;
+					break;
+				}
+			}
+			else if (dir == NorthWest){
+				if(movingCell->y %2 == 0 && access(n,map,movingCell->x-1,movingCell->y-1)){
+					movingCell->x=movingCell->x-1;
+					movingCell->y=movingCell->y-1;
+					break;
+				}
+				else if(movingCell->y %2 == 1 && access(n,map,movingCell->x,movingCell->y-1)){
+					movingCell->y=movingCell->y-1;
+					break;
+				}
+			}
+			else if (dir == SouthEast){
+				if(movingCell->y %2 == 1 && access(n,map,movingCell->x-1,movingCell->y+1)){
+					movingCell->x=movingCell->x+1;
+					movingCell->y=movingCell->y+1;
+					break;
+				}
+				else if(movingCell->y %2 == 0 && access(n,map,movingCell->x,movingCell->y+1)){
+					movingCell->y=movingCell->y+1;
+					break;
+				}
+			}
+			else if (dir == SouthWest){
+				if(movingCell->y %2 == 1 && access(n,map,movingCell->x-1,movingCell->y-1)){
+					movingCell->x=movingCell->x+1;
+					movingCell->y=movingCell->y-1;
+					break;
+				}
+				else if(movingCell->y %2 == 0 && access(n,map,movingCell->x,movingCell->y-1)){
+					movingCell->y=movingCell->y-1;
+					break;
+				}
+			}
+	}
+}
+
+removeCell ()
 
--- includes.h
+++ includes.h
@@ -4,12 +4,15 @@
 #include <string.h>
 #include <conio.h>
 #include <time.h>
+#include <windows.h>
 
 #define printMenu printf("1)Load\n2)New single player game\n3)New Multiplayer game\n4)Exit\n");
 #define printMoves printf("1)Move\n2)Split a Cell\n3)Boost Energy\n4)save\n5)Exit\n");
+#define compass printf("1)North\n2)South\n3)NorthEast\n4)NorthWest\n5)SouthEast\n6)SouthWest\n");
 #define clear system("cls");
 #define count(X) for(int i = 0;i < X;i++)
 #define for2(X) for(int i = 0;i < X;i++)for(int j = 0;j < X;j++)
+#define setRed SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),BACKGROUND_INTENSITY|BACKGROUND_RED);
 
 #ifndef _INCLUDES_H
 #define _INCLUDES_H
